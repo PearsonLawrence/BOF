8f2de8c9c43309c73f62b5351583b0ada52854db
justinkelly150 <70504900+justinkelly150@users.noreply.github.com>
2023-11-27 22:48:11 -0800

    edited circle, square shoot + movement

--- Assets/Scripts/Justin Scripts/FlyingEnemy.cs
+++ Assets/Scripts/Justin Scripts/FlyingEnemy.cs
@@ -11,12 +11,23 @@ public class FlyingEnemy : MonoBehaviour
     private GameObject player;
     public float circularMotionRadius = 2f;
     private float circularMotionAngle = 0f;
+    public LayerMask obstacleLayer;
+    public float avoidanceDistance = 1f;
+
+    Ray ray;
     
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, Vector3.right);
+
+        if (raycastHit2D)
+        {
+            Debug.Log("Something was hit!");
+        }
 
     }
 
@@ -40,6 +51,7 @@ public class FlyingEnemy : MonoBehaviour
         else
         {
             ReturnToStartPoint();
+
         }
         Flip();
             
@@ -47,8 +59,15 @@ public class FlyingEnemy : MonoBehaviour
     }
 
     private void Chase()
-    { 
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed*Time.deltaTime);
+    {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+            
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, avoidanceDistance, obstacleLayer);
+            if (hit.collider != null)
+            {
+                // Avoid obstacle by changing direction (turn 90 degrees)
+                transform.Rotate(Vector3.forward, 90f);
+            }
     }
 
     private void ReturnToStartPoint()
@@ -59,8 +78,14 @@ public class FlyingEnemy : MonoBehaviour
         float y = startingPoint.position.y + Mathf.Sin(circularMotionAngle);
 
         Vector2 circularMotionPosition = new Vector2(x, y);
-        
-        transform.position = Vector2.MoveTowards(transform.position, startingPoint.position, speed*Time.deltaTime);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, circularMotionPosition - (Vector2)transform.position, stoppingDistance, obstacleLayer);
+
+         if(hit.collider == null) 
+         { 
+             transform.position = Vector2.MoveTowards(transform.position, startingPoint.position, speed * Time.deltaTime);
+         } 
+
     }
 
     private void Flip()
@@ -70,4 +95,6 @@ public class FlyingEnemy : MonoBehaviour
         else
             transform.rotation = Quaternion.Euler(0, 180, 0);
     }
+
+    
 }
