99d59de91a283c55f22682a7c4756570dc315723
PearsonLawrence <pearson.lawrence.official@gmail.com>
2023-11-29 00:06:39 -0800

    Camera Shake and enemy damage animation
    As commit states

--- Assets/Scripts/Pearson Scripts/Misc/HealthComponent.cs
+++ Assets/Scripts/Pearson Scripts/Misc/HealthComponent.cs
@@ -7,9 +7,13 @@ public class HealthComponent : MonoBehaviour
 {
     [SerializeField]
     private float maxHealth;
-    
-
+  
     public float currentHealth;
+
+
+    [SerializeField] private CameraShake camShake;
+
+    [SerializeField] private Animator damageAnimator;
 
     [SerializeField] private GameObject deathParticlePrefab;
     [SerializeField] private GameObject dropPrefab;
@@ -19,16 +23,33 @@ public class HealthComponent : MonoBehaviour
 
 
 
-
     [SerializeField] private Image healthBar;
     public void Start()
     {
         currentHealth = maxHealth;
+        camShake = Camera.main.GetComponent<CameraShake>();
     }
 
+    public void EndDamageAnimation()
+    {
+        
+        damageAnimator.SetBool("isTakeDamage", false);
+       
+    }
     public void takeDamage(float amount)
     {
         currentHealth -= amount;
+
+        if(camShake != null && currentHealth > 0)
+        {
+            ShakeType type = (isPlayerHealth) ? ShakeType.playerDamage : ShakeType.enemyDamage;
+            camShake.doShake(type);
+        }
+
+        if(damageAnimator != null)
+        {
+            damageAnimator.SetBool("isTakeDamage", true);
+        }
 
         if(isPlayerHealth && healthBar != null)
         {
@@ -37,13 +58,18 @@ public class HealthComponent : MonoBehaviour
             if(dropPrefab != null) Instantiate(dropPrefab, transform.position, Quaternion.identity);
         }
 
-        if(currentHealth <= 0)
+        if(currentHealth <= 0 )
         {
             if (deathParticlePrefab != null) Instantiate(deathParticlePrefab, transform.position, Quaternion.identity);
 
             if(canDropPowerup && dropPrefab != null)
                 Instantiate(dropPrefab, transform.position, Quaternion.identity);
 
+            if (camShake != null)
+            {
+                ShakeType type = (isPlayerHealth) ? ShakeType.playerDeath : ShakeType.enemyDeath;
+                camShake.doShake(type);
+            }
             Destroy(this.gameObject);
         }
     }
