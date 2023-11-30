d70667e8006a0ca6a89f087fafb8a5fd3f8742c1
cblosch <blosce15@gmail.com>
2023-11-29 19:12:13 -0800

    Created procedural generation system

---
+++ Assets/Scripts/Colin Scripts/SpawnRooms.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRooms : MonoBehaviour
+{
+    public LayerMask whatIsRoom;
+    public LevelGeneration levelGen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, whatIsRoom);
+        if (roomDetection == null && levelGen.stopGeneration == true)
+        {
+            //spawn random room
+            int rand = Random.Range(0, levelGen.rooms.Length);
+            Instantiate(levelGen.rooms[rand], transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
