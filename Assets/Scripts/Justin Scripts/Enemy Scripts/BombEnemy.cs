7fd5b7a21e7136632b4e431e65318a365d69ec53
justinkelly150 <70504900+justinkelly150@users.noreply.github.com>
2023-12-12 03:56:43 -0800

    triangle mostly done, doesn't launch bomb projectile towards player

--- Assets/Scripts/Justin Scripts/Enemy Scripts/BombEnemy.cs
+++ Assets/Scripts/Justin Scripts/Enemy Scripts/BombEnemy.cs
@@ -1,64 +1,143 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 
 public class BombEnemy : MonoBehaviour
 {
-
-    public float speed = 1f;
-    public float rotation = 1f;
-    public Transform wall;
-    public Transform back;
-    public Transform front;
-    public Transform middle;
-    public GameObject Player;
-
-    private Collider2D frontBump;
-    private Collider2D middleBump;
-    private Collider2D backBump;
-    private Collider2D wallBump;
-
-    void Update()
+    private Rigidbody2D rb;
+    private bool groundDetected;
+    private bool wallDetected;
+    
+    [SerializeField]
+    private Transform groundPosCheck;
+    [SerializeField]
+    private Transform wallPosCheck;
+    
+    public float groundCheckDistance;
+    public float wallCheckDistance;
+    [SerializeField]
+    private LayerMask groundLayer;
+    private bool hasTurned;
+    private float zAxisAdd;
+    public float zAxisAngle;
+    private int direction;
+    public float groundOffset;
+    public float wallOffset;
+    public float speed;
+    
+    private void Start()
     {
-        frontBump = Physics2D.OverlapPoint(front.position, LayerMask.GetMask("ground"));
-        backBump = Physics2D.OverlapPoint(back.position, LayerMask.GetMask("ground"));
-        middleBump = Physics2D.OverlapPoint(middle.position, LayerMask.GetMask("ground"));
-        wallBump = Physics2D.OverlapPoint(wall.position, LayerMask.GetMask("ground"));
+        rb = GetComponent<Rigidbody2D>();
+        hasTurned = false;
+        direction = 1;
+    }
 
-        if(frontBump == null && middleBump == null && backBump == null && wallBump == null)
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGroundOrWall();
+        Movement();
+    }
+
+    void CheckGroundOrWall()
+    {
+        groundDetected = Physics2D.Raycast(groundPosCheck.position, -transform.up, groundCheckDistance, groundLayer);
+        wallDetected = Physics2D.Raycast(wallPosCheck.position, transform.right, wallCheckDistance, groundLayer);
+
+        if(!groundDetected)
         {
-            transform.Translate(new Vector3(speed, -1.5f * speed, 0f) * Time.deltaTime);
+            if(hasTurned == false)
+            {
+                zAxisAdd -= zAxisAngle;
+                transform.eulerAngles = new Vector3(0, 0, zAxisAdd);
+                
+                if(direction == 1)
+                {
+                    transform.position = new Vector2(transform.position.x + groundOffset, transform.position.y - groundOffset);
+                    hasTurned = true;
+                    direction = 2;
+                }
+                
+                if(direction == 2)
+                {
+                    transform.position = new Vector2(transform.position.x - groundOffset, transform.position.y - groundOffset);
+                    hasTurned = true;
+                    direction = 3;
+                }
+                
+                if(direction == 3)
+                {
+                    transform.position = new Vector2(transform.position.x - groundOffset, transform.position.y + groundOffset);
+                    hasTurned = true;
+                    direction = 4;
+                }
+
+                if (direction == 4)
+                {
+                    transform.position = new Vector2(transform.position.x + groundOffset, transform.position.y + groundOffset);
+                    hasTurned = true;
+                    direction = 1;
+                }
+
+            }
         }
-        else
-        {
-            if(wallBump != null)
-            {
-                transform.Rotate(0f, 0f, .8f * rotation, Space.Self);
-            }
-            else if(middleBump != null && frontBump == null)
-            {
-                transform.Rotate(0f, 0f, -1.5f * rotation, Space.Self);
-            }
-            else if(frontBump != null && middleBump == null && backBump == null)
-            {
-                transform.Rotate(0, 0, 1.5f * rotation, Space.Self);
-            }
 
-            if (middleBump == null && wallBump == null)
+        if(groundDetected)
+        {
+            hasTurned = false;
+        }
+
+        if(wallDetected)
+        {
+            if(hasTurned == false)
             {
-                transform.Translate(new Vector3(speed, 0f, 0f) * Time.deltaTime);
-            }
-            else if (middleBump == null && wallBump != null)
-            {
-                transform.Translate(new Vector3(-.2f * speed, -.2f * speed, 0f) * Time.deltaTime);
-            }
-            else if(middleBump != null && wallBump == null)
-            {
-                transform.Translate(new Vector3(1.25f * speed, speed, 0f) * Time.deltaTime);
+                zAxisAdd += zAxisAngle;
+                transform.eulerAngles = new Vector3(0, 0, zAxisAdd);
+                if(direction == 1)
+                {
+                    transform.position = new Vector2(transform.position.x - wallOffset , transform.position.y + wallOffset);
+                    hasTurned = true;
+                    direction = 4;
+                }
+                
+                if(direction == 2)
+                {
+                    transform.position = new Vector2(transform.position.x + wallOffset, transform.position.y + wallOffset);
+                    hasTurned = true;
+                    direction = 1;
+                }
+                
+                if(direction == 3)
+                {
+                    transform.position = new Vector2(transform.position.x + wallOffset, transform.position.y - wallOffset);
+                    hasTurned = true;
+                    direction = 2;
+                }
+                
+                if(direction == 4)
+                {
+                    transform.position = new Vector2(transform.position.x - wallOffset, transform.position.y - wallOffset);
+                    hasTurned = true;
+                    direction = 3;
+                }
             }
         }
     }
 
+    void Movement()
+    {
+        rb.velocity = transform.right * speed;
+    }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundPosCheck.position, new Vector2(groundPosCheck.position.x, groundPosCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallPosCheck.position, new Vector2(wallPosCheck.position.x + wallCheckDistance, wallPosCheck.position.y));
+    }
 }
 
---
+++ Assets/Scripts/Justin Scripts/Projectile Scripts/BombShoot.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombShoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject projectilePrefab;
+    public GameObject explosionPrefab;
+    public Transform bombPos;
+    public float shootInterval = 2f;
+    public float projectileSpeed = 5f;
+    public float explosionDelay = 2f;
+    public float explosionDuration = 1f;
+
+    private GameObject player;
+
+    void Start()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if(player == null)
+        {
+            Debug.LogError("Player not found in the scene.");
+        }
+        
+        StartCoroutine(ShootRoutine());    
+    }
+
+    IEnumerator ShootRoutine()
+    {
+        while (true)
+        {
+            ShootProjectile();
+            yield return new WaitForSeconds(shootInterval);
+        }
+
+        void ShootProjectile()
+        {
+            
+            if (player == null)
+            {
+                return;
+            }
+
+            Vector3 spawnPos = bombPos.TransformPoint(Vector3.zero);
+            GameObject projectile = Instantiate(projectilePrefab, spawnPos, Quaternion.identity);
+            Rigidbody2D projRb = projectile.GetComponent<Rigidbody2D>();
+
+            Vector2 directionToPlayer = (player.transform.position - transform.position).normalized;
+
+            projRb.velocity = directionToPlayer * projectileSpeed;
+
+            StartCoroutine(ExplodeAfterDelay(projectile));
+        }
+            
+        IEnumerator ExplodeAfterDelay(GameObject projectile)
+        {
+            yield return new WaitForSeconds(explosionDelay);
+
+            GameObject explosion = Instantiate(explosionPrefab, projectile.transform.position, Quaternion.identity);
+
+            Destroy(projectile);
+
+            StartCoroutine(MakeExplosionDisappear(explosion));
+        }
+
+        IEnumerator MakeExplosionDisappear(GameObject explosion)
+        {
+            yield return new WaitForSeconds(explosionDuration);
+
+            Destroy(explosion);
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
